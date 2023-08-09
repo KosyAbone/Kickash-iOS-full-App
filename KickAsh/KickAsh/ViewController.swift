36774acffdef527639c3064f4c4505f02dc167c9
Ilham Sheikh <ilhamsheikh@Ilhams-MacBook-Pro.local>
2023-08-09 00:50:19 -0400

    Added UI for Questionnaire

--- KickAsh/KickAsh/ViewController.swift
+++ KickAsh/KickAsh/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var QuestionnaireStartButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
